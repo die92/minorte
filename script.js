1294f2ef2604e58f79ca71698374542d1d563065
diego <diegopezzano20@gmail.com>
2022-12-29 17:12:22 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let tar = document.querySelector('.desplegar')
+tar.onclick = function(){
+    let tarjetas = document.querySelector('.tarjetas')
+    tarjetas.classList.toggle('desplegadas')
+    
+}
+
